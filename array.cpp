10711860429282a25b2b86f7d60639243cad998b
kevinno.limiy@gmail.com <kevinno.limiy@gmail.com>
2026-04-07 16:33:47 +0700

    cara menulis atau mengisi array

--- array.cpp
+++ array.cpp
@@ -13,6 +13,10 @@ int main()
     // cara membaca array dataku
     cout << "Data ke - 3 : " << dataku[2] << endl;
 
-        // Operasi Matematika
+    // Operasi Matematika
     x = dataku[2] * 3;
+
+        // cara menulis atau mengisi array
+    dataku[2] = 600;
+    cout << "Data ke - 3 yang baru : " << dataku[2] << endl;
 }
